b17e4c932ca1c2585f869dc98474bbd914e844ab
Humayun63 <humayun126224@gmail.com>
2023-08-05 11:19:44 +0600

    pages and router connection setup

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,6 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/Main";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Landing from "../pages/LandingPage/Landing";
+import Home from "../pages/HomePage/Home";
+import Login from "../pages/LoginPage/Login";
+import Register from "../pages/RegisterPage/Register";
+import Profile from "../pages/ProfilePage/Profile";
+import EasyProblems from "../pages/EasyProblemPage/EasyProblems";
+import MediumProblems from "../pages/MediumProblemPage/MediumProblems";
+import AdvanceProblems from "../pages/AdvanceProblemPage/AdvanceProblems";
 
 const router = createBrowserRouter([
     {
@@ -10,7 +18,35 @@ const router = createBrowserRouter([
         children:[
             {
                 path:'/',
-                element:<h2>hello</h2>
+                element:<Landing></Landing>
+            },
+            {
+                path:'home',
+                element:<Home></Home>
+            },
+            {
+                path:'login',
+                element:<Login></Login>
+            },
+            {
+                path:'register',
+                element:<Register></Register>
+            },
+            {
+                path:'profile',
+                element:<Profile></Profile>
+            },
+            {
+                path:'easy-problems',
+                element:<EasyProblems></EasyProblems>
+            },
+            {
+                path:'medium-problems',
+                element:<MediumProblems></MediumProblems>
+            },
+            {
+                path:'advance-problems',
+                element:<AdvanceProblems></AdvanceProblems>
             }
         ]
     },
