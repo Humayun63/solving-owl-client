0cfdd2b7db5bac88572863ce9d0fb7e4bb390f40
Humayun63 <humayun126224@gmail.com>
2023-08-06 08:35:22 +0600

    code from user working perfectly

--- src/pages/SingleProblemPage/SingleProblemPage.jsx
+++ src/pages/SingleProblemPage/SingleProblemPage.jsx
@@ -2,15 +2,69 @@ import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Loader from '../../components/Loader/Loader';
 import CodeEditor from '@uiw/react-textarea-code-editor';
+import Swal from 'sweetalert2';
+
 
 const SingleProblemPage = () => {
     const loadedProblem = useLoaderData() || [];
-    const { title, sub_title } = loadedProblem[0] || {}
+    const { title, sub_title, tests } = loadedProblem[0] || {}
     const [code, setCode] = useState('');
+    const [output, setOutput] = useState([])
 
-    const handleSubmit = () =>{
+    const handleSubmit = event => {
         event.preventDefault()
-        console.log(code)
+        let inputCode;
+        let result;
+        setOutput([])
+
+
+        if (code.trim().split('')[0] !== 'function') {
+            let array = code.trim().split('')
+            if (array[array.length - 1] === ';') {
+                array.pop()
+            }
+            let index = array.indexOf('=')
+            array.splice(0, index + 1)
+            inputCode = array.join('')
+        } else {
+            inputCode = code;
+        }
+        console.log(inputCode);
+
+        for (let item of tests) {
+            try {
+                result = eval(`(${inputCode})(${item.input})`)
+                if (item.output !== result) {
+                    console.log('failed');
+                    setOutput((output) => [...output, `Failed: ${item.fail} not ${result}`])
+                    setAllResult(false)
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something is wrong in your code!'
+                    })
+                    break;
+                } else if (item === tests[tests.length - 1]) {
+                    setOutput((output) => [...output, 'Passed'])
+                    Swal.fire({
+                        position: 'center-center',
+                        icon: 'success',
+                        title: 'Your work has been saved',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                } else {
+                    setOutput((output) => [...output, 'Passed'])
+                }
+            } catch(error){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: error.message
+                })
+            }
+        }
+
     }
     return (
         <>
@@ -34,8 +88,14 @@ const SingleProblemPage = () => {
                                     fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
                                 }}
                             />
-                            <input type="submit" value="Submit" className='px-4 py-2 rounded-md mt-4 text-black bg-[#A2F740] cursor-pointer'/>
+                            <input type="submit" value="Submit" className='px-4 py-2 rounded-md mt-4 text-black bg-[#A2F740] cursor-pointer' />
                         </form>
+                        <div className='py-4'>
+                            <h3 className='text-xl font-medium'>Output:</h3>
+                            <ul className='list-disc ms-8 mt-4'>
+                                {output.map((item, index) => <li key={index} className={`text-xl ${item === 'Passed' ? 'text-[#A2F740]' : 'text-red-500'}`}>{item}</li>)}
+                            </ul>
+                        </div>
                     </div>
                 </>
             }
